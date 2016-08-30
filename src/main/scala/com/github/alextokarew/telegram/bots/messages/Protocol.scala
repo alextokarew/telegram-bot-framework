47ae179f595d7ad39ad520a007af2e89678b4ee5
Alexander Tokarev <alex.tokarew@gmail.com>
2016-08-30 22:36:44 +0300

    Renaming GetMeResult to User

--- src/main/scala/com/github/alextokarew/telegram/bots/messages/Protocol.scala
+++ src/main/scala/com/github/alextokarew/telegram/bots/messages/Protocol.scala
@@ -1,14 +1,14 @@
 package com.github.alextokarew.telegram.bots.messages
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.github.alextokarew.telegram.bots.messages.Protocol.Responses.{GetMe, GetMeResult}
+import com.github.alextokarew.telegram.bots.messages.Protocol.Responses.{GetMe, User}
 import spray.json.DefaultJsonProtocol
 
 /**
   * Created by alextokarew on 30.08.16.
   */
 trait Protocol extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val getMeResultFormat = jsonFormat3(GetMeResult)
+  implicit val userFormat = jsonFormat4(User)
   implicit val getMeFormat = jsonFormat2(GetMe)
 
 }
@@ -19,7 +19,7 @@ object Protocol {
     /**
       * {"ok":true,"result":{"id":267503591,"first_name":"A super useful Bot","username":"asuperusefulbot"}}
       */
-    case class GetMe(ok: Boolean, result: GetMeResult)
-    case class GetMeResult(id: Long, first_name: String, username: String)
+    case class GetMe(ok: Boolean, result: User)
+    case class User(id: Long, first_name: String, last_name: Option[String], username: Option[String])
   }
 }
