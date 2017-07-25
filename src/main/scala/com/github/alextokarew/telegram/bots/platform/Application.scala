9c1e18f79823d7df6be0885a8df8b47b7ad72bad
a.tokarev <a.tokarev@cleverdata.ru>
2017-07-25 11:09:00 +0300

    Fix main entry point

--- src/main/scala/com/github/alextokarew/telegram/bots/platform/Application.scala
+++ src/main/scala/com/github/alextokarew/telegram/bots/platform/Application.scala
@@ -9,25 +9,28 @@ import com.typesafe.config.ConfigFactory
 /**
   * Created by alextokarew on 30.08.16.
   */
-object Application extends App with Protocol {
-  println("Starting bot's backend") //TODO log
+object Application extends Protocol {
 
-  implicit val system = ActorSystem("asuperusefulbot")
-  implicit val executor = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+  def main(args: Array[String]): Unit = {
+    println("Starting bot's backend") //TODO log
 
-  val config = ConfigFactory.load()
-  val token = config.getString("telegram.bot.token")
-  val apiConnector = TelegramApiConnector(config.getConfig("telegram.api.http"))
+    implicit val system = ActorSystem("asuperusefulbot")
+    implicit val executor = system.dispatcher
+    implicit val materializer = ActorMaterializer()
 
-  val url = config.getString("telegram.api.url").replace("<token>", token)
-  val timeout = config.getInt("telegram.poller.timeout")
+    val config = ConfigFactory.load()
+    val token = config.getString("telegram.bot.token")
+    val apiConnector = TelegramApiConnector(config.getConfig("telegram.api.http"))
 
-  val printer = system.actorOf(Props[Printer])
-  val router = system.actorOf(Router.props())
+    val url = config.getString("telegram.api.url").replace("<token>", token)
+    val timeout = config.getInt("telegram.poller.timeout")
+
+    val printer = system.actorOf(Props[Printer])
+    val router = system.actorOf(Router.props())
 // TODO: val poller = system.actorOf(Poller.props(url, timeout, router, apiConnector))
 
-  sys.addShutdownHook {
-    system.terminate()
+    sys.addShutdownHook {
+      system.terminate()
+    }
   }
 }
